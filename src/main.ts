962ad765d9983ad0d75dbad98b3557fe8f4e863d
Frendzlu <mateuszfrancik.mtf@gmail.com>
2022-07-22 14:34:29 +0200

    complete maze data structure

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import {Geometry} from "./Geometry";
 import Point = Geometry.Point;
 
 let x = new Maze(17, {
-	minimalRoomSize: 1/6,
-	roomNumberMultiplier: 3,
-	initialRoomNumber: 5
+	minimalRoomSize: 1/8,
+	roomNumberMultiplier: 2,
+	initialRoomNumber: 6
 })
 console.log(x)
 
@@ -17,7 +17,7 @@ render(x, canvas)
 function render(maze: Maze, canvas: HTMLCanvasElement) {
 	let ctx = canvas.getContext("2d")!
 	let minDim = (canvas.width < canvas.height ? canvas.width : canvas.height)
-	let floorHeight = ((minDim * 0.90) / 2) / maze.floorNumber
+	let floorHeight = ((minDim * 0.90) / 2) / maze.floorAmount
 	let center = new Point(canvas.width / 2, canvas.height/2)
 	let centerpieceRadius = minDim * 0.05
 	console.log(centerpieceRadius, floorHeight)
@@ -26,19 +26,25 @@ function render(maze: Maze, canvas: HTMLCanvasElement) {
 
 	for (let floor of maze.floors) {
 		ctx.beginPath()
-		ctx.arc(center.x, center.y, centerpieceRadius + floor.floorId * floorHeight, 0, 2*Math.PI)
+		ctx.arc(center.x, center.y, centerpieceRadius + floor.floorNumber * floorHeight, 0, 2*Math.PI)
 		ctx.stroke()
 
-		let initialLowPoint = new Geometry.Point(center.x, center.y + centerpieceRadius + (floor.floorId - 1) * floorHeight)
-		let initialHighPoint = new Geometry.Point(center.x, center.y + centerpieceRadius + floor.floorId * floorHeight)
+		let initialLowPoint = new Geometry.Point(center.x, center.y - (centerpieceRadius + (floor.floorNumber - 1) * floorHeight))
+		let initialHighPoint = new Geometry.Point(center.x, center.y - (centerpieceRadius + floor.floorNumber * floorHeight))
 		let currentLowPoint = initialLowPoint
 		let currentHighPoint = initialHighPoint
 		let angle = 360 / floor.rooms.length
 
 		for (let i = 1;  i <= floor.rooms.length; i++) {
+			ctx.beginPath()
 			ctx.moveTo(currentLowPoint.x, currentLowPoint.y)
 			ctx.lineTo(currentHighPoint.x, currentHighPoint.y)
 			ctx.stroke()
+			ctx.beginPath()
+			ctx.strokeStyle = "red"
+			if (i == 1) ctx.arc(currentLowPoint.x + 10, currentLowPoint.y - 10, 5, 0, 2*Math.PI)
+			ctx.stroke()
+			ctx.strokeStyle = "black"
 			currentLowPoint = initialLowPoint.rotateAlong(angle * i, center)
 			currentHighPoint = initialHighPoint.rotateAlong(angle * i, center)
 		}
--- src/testGenerator.ts
+++ src/testGenerator.ts
@@ -1,39 +1,81 @@
-class Room {
-	connections: Room[]
-	id: number
+interface RoomId {
+	floorNumber: number
+	roomNumber: number
+}
 
-	constructor(roomId: number) {
-		this.id = roomId
+interface RoomConnection {
+	roomId: RoomId
+	isOpen: boolean
+}
+
+class Room {
+	connections: RoomConnection[]
+	id: RoomId
+
+	constructor(floorId: number, roomId: number) {
+		this.id = {
+			floorNumber: floorId,
+			roomNumber: roomId
+		}
 		this.connections = []
 	}
 
-	addConnection(room: Room) {
-		this.connections.push(room)
+	addConnection(floorNumber: number, roomNumber: number, open: boolean = false) {
+		this.connections.push({
+			roomId: {
+				floorNumber: floorNumber,
+				roomNumber: roomNumber
+			},
+			isOpen: open
+		})
 	}
 
-	removeConnection(room: Room) {
-		this.connections.splice(this.connections.indexOf(room), 1)
+	removeConnection(roomId: RoomId) {
+		let roomConnection = this.connections.find(conn => conn.roomId.roomNumber === roomId.roomNumber && conn.roomId.floorNumber === roomId.floorNumber)
+		if (roomConnection) {
+			this.connections.splice(this.connections.indexOf(roomConnection), 1)
+		}
 	}
-
 }
 
 class RoomFactory {
-	currentRoomId = 0
+	currentRoomNumber = 0
+	floorNumber: number
+	roomAmount: number
 
-	createRoom() {
-		this.currentRoomId++
-		return new Room(this.currentRoomId)
+	constructor(floorNumber: number, roomAmount: number) {
+		this.floorNumber = floorNumber
+		this.roomAmount = roomAmount
+	}
+
+
+	createRoom(multiplier: number, numberOfOutwardConnections: number) {
+		this.currentRoomNumber++
+		let room = new Room(this.floorNumber, this.currentRoomNumber)
+		room.addConnection(this.floorNumber, this.currentRoomNumber < this.roomAmount ? this.currentRoomNumber+1 : 1)
+		room.addConnection(this.floorNumber, this.currentRoomNumber > 1 ? this.currentRoomNumber-1 : this.roomAmount)
+		if (this.floorNumber > 1) {
+			room.addConnection(this.floorNumber - 1, Math.ceil(this.roomAmount / multiplier))
+		}
+		if (numberOfOutwardConnections > 0) {
+			for (let i = 0; i < numberOfOutwardConnections; i++) {
+				room.addConnection(this.floorNumber + 1, this.currentRoomNumber + i)
+			}
+		}
+		return room
 	}
 }
 
 class Floor {
 	rooms: Room[]
-	floorId: number
+	floorNumber: number
 
-	constructor(numberOfRooms: number, floorId: number) {
-		let rf = new RoomFactory()
-		this.floorId = floorId
-		this.rooms = Array(numberOfRooms).fill(undefined).map(() => rf.createRoom())
+	constructor(numberOfRooms: number, floorNumber: number, roomNumberMultiplier: number, outwardConnectionsAmount: number) {
+		let rf = new RoomFactory(floorNumber, numberOfRooms)
+		this.floorNumber = floorNumber
+		this.rooms = Array(numberOfRooms)
+			.fill(undefined)
+			.map(() => rf.createRoom(roomNumberMultiplier, outwardConnectionsAmount))
 	}
 }
 
@@ -54,30 +96,36 @@ class FloorFactory {
 	currentRoomNumber: number
 	minRoomSize: number
 	roomNumberMultiplier: number
+	maxFloors: number
 
-	constructor(options: Partial<FloorOptions> = defaultFloorOptions) {
+	constructor(maxFloors: number, options: Partial<FloorOptions> = defaultFloorOptions) {
 		this.minRoomSize = options.minimalRoomSize || defaultFloorOptions.minimalRoomSize
 		this.currentRoomNumber = options.initialRoomNumber || defaultFloorOptions.initialRoomNumber
 		this.roomNumberMultiplier = options.roomNumberMultiplier || defaultFloorOptions.roomNumberMultiplier
+		this.maxFloors = maxFloors
 	}
 
 	createFloor() {
 		this.currentFloorId++
-		//console.log(this.currentFloorId / this.currentRoomNumber*2)
-		if (this.currentFloorId / (this.currentRoomNumber * 2) >= this.minRoomSize) {
+		let outwardConnectionsAmount = 1
+		while (this.currentFloorId / (this.currentRoomNumber * this.roomNumberMultiplier) >= this.minRoomSize) {
 			this.currentRoomNumber *= this.roomNumberMultiplier
 		}
-		return new Floor(this.currentRoomNumber, this.currentFloorId)
+		if ((this.currentFloorId + 1) / (this.currentRoomNumber * this.roomNumberMultiplier) >= this.minRoomSize) {
+			outwardConnectionsAmount = this.roomNumberMultiplier
+		}
+		if (this.currentFloorId === this.maxFloors) outwardConnectionsAmount = 0
+		return new Floor(this.currentRoomNumber, this.currentFloorId, this.roomNumberMultiplier, outwardConnectionsAmount)
 	}
 }
 
 export class Maze {
-	floorNumber: number
+	floorAmount: number
 	floors: Floor[]
 
-	constructor(floorNumber: number, options?: Partial<FloorOptions>) {
-		this.floorNumber = floorNumber
-		let ff = new FloorFactory(options)
-		this.floors = Array(floorNumber).fill(undefined).map(() => ff.createFloor())
+	constructor(floorAmount: number, options?: Partial<FloorOptions>) {
+		this.floorAmount = floorAmount
+		let ff = new FloorFactory(floorAmount, options)
+		this.floors = Array(floorAmount).fill(undefined).map(() => ff.createFloor())
 	}
 }
