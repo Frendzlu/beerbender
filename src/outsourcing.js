1390d44ccdeaf5c9863736c33182fa35d923d4e4
Frendzlu <mateuszfrancik.mtf@gmail.com>
2022-07-22 14:54:38 +0200

    deleted unnecessary files

--- src/outsourcing.js
+++
@@ -1,4 +0,0 @@
-//  https://github.com/codebox/mazes/blob/main/js/view.js - maze generator
-// TODO: zajebać kod z powyższego
-/// https://github.com/malik-tillman/Fluid-JS - fluid dynamics
-// TODO: zobaczyć czy działa
